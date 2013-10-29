4a4dc450b3c89c3f462d7b7033048ef497d9f22d
Nicks <messygranthor@gmail.com>
2013-10-29 09:59:11 -0700

    working with function and vars

---
+++ day1.py
@@ -0,0 +1,18 @@
+my_switch = 3
+
+if my_switch == 1:
+    print "bwhahahah"
+else:
+    print "nooooooooooooo"
+print "hello world!"
+print 7352 / 17
+print "my name is bob"
+print "i am your computer"
+if my_switch == 1:
+    print "aegfla"
+elif my_switch == 2:
+    print 1 + 67
+elif my_switch == 3:
+    print 6 * 9
+else: 
+    print "that is not a valid command"
---
+++ day2.py
@@ -0,0 +1,16 @@
+def hello_class():
+    print "hello, welcome nick, to class"
+    return "1.5"
+
+def f1():
+    print 9 * 6
+
+def f2(name):
+    print name
+    print type (name)
+    name = "hi" 
+
+my_name = "nick"
+print f2(my_name)
+print my_name
+
